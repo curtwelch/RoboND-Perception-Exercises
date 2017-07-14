317d170e911da786e7b329f514b481318f076a30
Curt Welch <curt@kcwc.com>
2017-07-14 10:11:27 -0700

    Add point clould and pass though filters to RANSAC.ph
    Working on class exercise.

--- Exercise-1/RANSAC.py
+++ Exercise-1/RANSAC.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # Import PCL module
 import pcl
 
@@ -5,9 +7,39 @@ import pcl
 cloud = pcl.load_XYZRGB('tabletop.pcd')
 
 # Voxel Grid filter
+# Create a VoxelGrid filter object for our input point cloud
+vox = cloud.make_voxel_grid_filter()
+
+# Choose a voxel (also known as leaf) size
+# Note: this (1) is a poor choice of leaf size   
+# Experiment and find the appropriate size!
+LEAF_SIZE = 0.01   
+
+# Set the voxel (or leaf) size  
+vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+
+# Call the filter function to obtain the resultant downsampled point cloud
+cloud_filtered = vox.filter()
+filename = 'v%dmm.pcd' % (int(LEAF_SIZE*1000),)
+print "file name is", filename
+pcl.save(cloud_filtered, filename)
 
 
 # PassThrough filter
+# Create a PassThrough filter object.
+passthrough = cloud_filtered.make_passthrough_filter()
+
+# Assign axis and range to the passthrough filter object.
+filter_axis = 'z'
+passthrough.set_filter_field_name (filter_axis)
+axis_min = .7
+axis_max = 1.1
+passthrough.set_filter_limits (axis_min, axis_max)
+
+# Finally use the filter function to obtain the resultant point cloud. 
+cloud_filtered = passthrough.filter()
+filename = 'pass_through_filtered.pcd'
+pcl.save(cloud_filtered, filename)
 
 
 # RANSAC plane segmentation
